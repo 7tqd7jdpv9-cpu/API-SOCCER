e394c568ed50f7f6daea30faff293803b6a536b5
7tqd7jdpv9-cpu <7tqd7jdpv9@privaterelay.appleid.com>
2026-07-20 10:12:52 +0900

    game.js を更新

--- js/game.js
+++ js/game.js
@@ -20,9 +20,35 @@ playBtn.onclick = () => {
     ctx.lineWidth = 5;
     ctx.strokeRect(canvas.width/2 - 80, 30, 160, 60);
 
+    let ball = {
+    x: canvas.width / 2,
+    y: canvas.height - 100,
+    r: 25
+};
+
+function draw() {
+    // 芝生
+    ctx.fillStyle = "#1d8f45";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // ゴール
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(canvas.width / 2 - 80, 30, 160, 60);
+
     // ボール
     ctx.beginPath();
     ctx.fillStyle = "white";
-    ctx.arc(canvas.width/2, canvas.height - 100, 25, 0, Math.PI * 2);
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
     ctx.fill();
+}
+
+draw();
+
+canvas.addEventListener("touchmove", (e) => {
+    const touch = e.touches[0];
+    ball.x = touch.clientX;
+    ball.y = touch.clientY;
+    draw();
+});
 };
